f10ac498f7690faba772c905ccb01c30272ba954
Manjinderjit <manjindergrewal1997@gmail.com>
2025-12-17 01:37:02 -0800

    added player movement

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 from circleshape import CircleShape
 
@@ -27,10 +27,19 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_RIGHT]:
             self.rotate(dt)
+            
+        if keys[pygame.K_UP]:
+            self.move(dt)
+            
+        if keys[pygame.K_DOWN]:
+            self.move(-dt)    
         
-        
-        
-    
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
     # he Player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
