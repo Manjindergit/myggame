1f1f5f11bf77194c5ab3ef9ebd110940cfe08f2d
Manjinderjit <manjindergrewal1997@gmail.com>
2025-12-17 00:25:23 -0800

    enabled display

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
-    print(f"Screen dimensions: {SCREEN_WIDTH}x{SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids Game")
+    
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
