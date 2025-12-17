0681faf67d04e71bb2425fa437228e963f66d743
Manjinderjit <manjindergrewal1997@gmail.com>
2025-12-16 23:57:11 -0800

    added logging

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
-    print("Hello from myggame!")
+    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen dimensions: {SCREEN_WIDTH}x{SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
